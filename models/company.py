ef1e81fac2da75cb3e0dcb1f6083ba35d352b740
Keiver Peña <keiverp.robles@gmail.com>
2020-09-23 19:12:39 -0400

    [IMP] l10n_ve_dpt: Ajusta campos municipio y parroquia al mismo nivel
    * [REF] Ajusta el manifest y etiqueta data en xml
    * [FIX] Referencia del país es por IDX y no por ID
    * Elimina espacios en blanco al final de algunas entidades.

--- models/company.py
+++ models/company.py
@@ -1,16 +1,9 @@
 # -*- coding: utf-8 -*-
-
-
-from odoo import api, models, fields, _ 
-
-
+from odoo import models, fields
 
 
 class Company(models.Model):
     _inherit = 'res.company'
 
-
     municipality_id = fields.Many2one('res.country.state.municipality', 'Municipality')
     parish_id = fields.Many2one('res.country.state.municipality.parish', 'Parish')
-
-
