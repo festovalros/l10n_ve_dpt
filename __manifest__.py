d4a2c921001f08d57cd4ce37537c07008f1fac1b
Samuel Pavon <festovalros@gmail.com>
2025-09-22 15:52:08 -0400

    [MIG] migrate to V17

--- __manifest__.py
+++ __manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     "name": "Localización Politico Territorial Venezolana: Municipios y Parroquias",
-    "version": "15.0.0.0.1",
+    "version": "17.0.0.0.1",
     "author": "Empero LLC",
     "category": "Localization",
     "description":
